44c47c5a499e6e28b88bf2f9a768dcf7306fcb72
mahonch <maxim.p811@mail.ru>
2024-05-07 08:58:39 +0300

    Post.kt Header

--- app/src/main/java/com/topic3/android/reddit/screens/HomeScreen.kt
+++ app/src/main/java/com/topic3/android/reddit/screens/HomeScreen.kt
@@ -1,8 +1,8 @@
 package com.topic3.android.reddit.screens
-
+import com.topic3.android.reddit.viewmodel.MainViewModel
 import androidx.compose.runtime.Composable
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(viewModel:MainViewModel) {
 
 }
